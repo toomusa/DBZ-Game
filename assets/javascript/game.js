f56c5fa6dd26d7370ea9badcff94b8eae97290bc
toomusa <3895499+toomusa@users.noreply.github.com>
2019-05-30 22:11:13 -0700

    updated page layout and pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,6 +17,54 @@ let opponent = {
     }
 }
 
+let goku = {
+    health: 1400,
+    counter: 50,
+    power: {
+        melee: 120,
+        energy: 400
+    }
+};
+
+let vegeta = {
+    health: 800,
+    counter: 100,
+    power: {
+        melee: 80,
+        energy: 300
+    }
+};
+
+let frieza = {
+    health: 1200,
+    counter: 70,
+    power: {
+        melee: 90,
+        energy: 500
+    }
+};
+
+let jiren = {
+    health: 1300,
+    counter: 200,
+    power: {
+        melee: 50,
+        energy: 600
+    }
+};
+
+let beerus = {
+    health: 1500,
+    counter: 150,
+    power: {
+        melee: 200,
+        energy: 700
+    }
+};
+
+// executes melee attack, deals damange, receives counter damage
+// includes gameOver check and restartGame
+
 const meleeAttack = () => {
 
     let meleeButton = document.getElementById("melee-button");
@@ -54,6 +102,9 @@ const meleeAttack = () => {
     }, 200)
 }
 
+// executes energy attack, deals damange, receives counter damage
+// includes gameOver check and restartGame
+
 const energyAttack = () => {
 
     let energyButton = document.getElementById("energy-button");
@@ -90,14 +141,20 @@ const energyAttack = () => {
     }, 2000)
 }
 
+// calculate attack damage at random
+
 const attackDamage = (power) => {
     return Math.floor(Math.random() * power);
 }
+
+// boolean check for is the game over
 
 const gameOver = (health) => {
     // health = 0;
     return health <= 0;
 }
+
+// resets buttons and prints message
 
 const endGame = (message) => {
     document.getElementById("marquee").textContent = message;
@@ -105,6 +162,8 @@ const endGame = (message) => {
     document.getElementById("energy-button").hidden = true;
     document.getElementById("restart-button").hidden = false;
 }
+
+// resets the game 
 
 const restartGame = () => {
     let meleeButton = document.getElementById("melee-button");
@@ -120,6 +179,8 @@ const restartGame = () => {
     document.getElementById("restart-button").hidden = true;
 }
 
+// displays health information on screen 
+
 const printToScreen = () => {
     document.getElementById("player-health").textContent = player.health;
     document.getElementById("opponent-health").textContent = opponent.health;
@@ -127,3 +188,39 @@ const printToScreen = () => {
 
 printToScreen();
 
+
+
+// part of the restartGame function
+
+    // select character and assign to player
+
+    // select character and assign to opponent
+    
+
+// insert character images to character selector
+
+    // create 4 states: ready, player, opponent, dead
+
+    // move character to correct box based on state
+
+
+// enchance game mechanics
+
+    // miss a turn after using energy attack (double counter)
+
+    // miss an attack if random attack power is less than 20
+
+
+    // on character click
+    // if playerPick and enemyPick is false, pick a fighter
+    // character id removed to pick can't be repicked
+    // when dead, set enemyPick to false
+    // assign a class to defeated enemy
+    // to change image, use .html with img tags in quotes
+    // defeat enemy, go super saiyan
+    // gameEnd function for end game or pick next enemy
+    // location.reload(true); to reload page
+
+// additional features
+
+    // on image hover, show fighter stats using modal
